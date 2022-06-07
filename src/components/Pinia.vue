bc37589b50891442d697ec318f40b27c9d8e0503
yejun <yj@juwan.group>
2022-06-07 18:50:10 +0800

    feat: pinia use state getters actions

--- src/components/Pinia.vue
+++ src/components/Pinia.vue
@@ -1,6 +1,9 @@
 <template>
   <p>counter: {{ store.counter }}</p>
   <button @click="handleClick">counter+1</button>
+  <p>msg: {{ msg }}</p>
+  <p>doubleCounter {{ doubleCounter }}</p>
+  <p>doubleMsg {{ doubleMsg }}</p>
 </template>
 
 <script setup>
@@ -9,11 +12,26 @@ import { storeToRefs } from 'pinia'
 
 const store = mainStore()
 
-// console.log('pinia', store)
+console.log('pinia', store)
 
-const { counter } = storeToRefs(store)
+const { counter, msg, doubleCounter, doubleMsg } = storeToRefs(store)
+// console.log('storeToRefs', storeToRefs(store))
 
 function handleClick() {
   counter.value++
+  store.$patch({
+    msg: 'pinia good!',
+    counter: counter.value + 1,
+  })
+
+  store.$patch((state) => {
+    console.log('state', state)
+  })
+
+  // store.increment('yejun') // 加2+yejun
+
+  setTimeout(() => {
+    store.$reset() // 重置到默认值
+  }, 1000)
 }
 </script>
--- src/pinia/index.js
+++ src/pinia/index.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia"
 
+/* 第一个参数为容器的id */
 export const mainStore = defineStore('main', {
   state: () => {
     return {
@@ -8,9 +9,16 @@ export const mainStore = defineStore('main', {
     }
   },
   getters: {
-
+    doubleCounter(state) {
+      return state.counter * 2
+    },
+    doubleMsg() {
+      return this.msg + this.msg
+    }
   },
   actions: {
-
+    increment(name) {
+      this.counter = this.counter + 2 + name
+    },
   }
 })
